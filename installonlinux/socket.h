d7b03a4924e74aca90f52ecf5ed6b22feadb4cc6
Unam3dd <34691959+Unam3dd@users.noreply.github.com>
2020-09-02 22:48:57 +0200

    Update socket.h

--- installonlinux/socket.h
+++ installonlinux/socket.h
@@ -9,10 +9,16 @@ typedef struct{
     #pragma comment (lib,"Ws2_32.lib")
     #include <winsock2.h>
     #include <ws2tcpip.h>
+    #include <stdio.h>
     #include <windows.h>
+
+    
+    fd_set Write,Err;
+    struct timeval t;
 
     SOCKET Socket(short family,int type,int protocol);
     int Connect(SOCKET fd,const char *host,int port);
+    int ConnectTimeout(SOCKET fd,const char *host, int port, int ms);
     int Bind(SOCKET fd,int port);
     int Listen(SOCKET fd,int maxcon);
     SOCKET Accept(SOCKET fd);
@@ -41,22 +47,21 @@ typedef struct{
 
     typedef int SOCKET;
     typedef struct sockaddr_in SOCKADDR_IN;
-    typedef struct sockaddr SOCKADDR;
 
     SOCKET Socket(short family,int type,int protocol);
     int Connect(SOCKET fd,const char *host,int port);
+    int ConnectTimeout(SOCKET fd,const char *host, int port, int ms);
     int Bind(SOCKET fd,int port);
     int Listen(SOCKET fd,int maxcon);
     SOCKET Accept(SOCKET fd);
     int Send(SOCKET fd,const char *data,int flags);
-    int SendTimeout(SOCKET fd,const char *data,int seconds,int flags);
+    int SendTimeout(SOCKET fd,const char *data,unsigned long ms,int flags);
     int RecvData(SOCKET fd,char *buffer,int bytes,int flags);
-    int RecvDataTimeout(SOCKET fd,char *buffer,int bytes,int flags,int seconds);
+    int RecvDataTimeout(SOCKET fd,char *buffer,int bytes,int flags,unsigned long ms);
     peer_t GetPeerName(SOCKET fd);
     SOCKADDR_IN session(const char *host,int port);
     SOCKADDR_IN sessionbind(short family,int port);
     void ExecuteAndStreamProcess(SOCKET fd,const char *process);
-    int rdata(SOCKET s, char* buffer, int len,int flags);
 
     
 
